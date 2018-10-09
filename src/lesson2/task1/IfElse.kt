6f8bac5e12105da92bd91404a6cb70e1087b1687
BigArtemka <dak.12008@gmail.com>
2018-10-09 14:42:04 +0300

    Переделано последнее задание lesson 2 task 1

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -171,8 +171,6 @@ fun triangleKind(a: Double, b: Double, c: Double): Int {
  * Если пересечения нет, вернуть -1.
  */
 fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
-    return when {
-        (min(b, d) - max(a, c) >= 0) -> min(b, d) - max(a, c)
-        else -> -1
-    }
+    return if (min(b, d) - max(a, c) >= 0) min(b, d) - max(a, c)
+    else -1
 }
