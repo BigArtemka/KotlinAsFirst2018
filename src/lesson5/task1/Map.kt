57ffda890260f2148539f5e743248976859176fc
BigArtemka <dak.112008@gmail.com>
2018-10-31 14:34:08 +0300

    lesson 5

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -292,7 +292,7 @@ fun hasAnagrams(words: List<String>): Boolean = TODO()
  */
 fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
     for (i in list)
-        if ((list - i).contains(number - i)) return Pair(list.indexOf(i), (list - i).indexOf(number - list[i]) + 1)
+        if ((list - i).contains(number - i)) return Pair(list.indexOf(i), (list - i).indexOf(number - i) + 1)
     return Pair(-1, -1)
 }
 
